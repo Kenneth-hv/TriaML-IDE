6d243e6dea2c4bb1ed1b46287ca62d05faadc5ef
Kenneth Herrera <kfhv.24@gmail.com>
2021-03-28 21:58:31 -0600

    Added Monaco Editor
    https: //github.com/Microsoft/monaco-editor
    Co-Authored-By: Esteban Jiménez <44732656+JesusJimenezValverde@users.noreply.github.com>
    Co-Authored-By: josebuddy <33288118+josebuddy@users.noreply.github.com>

--- src/components/tabPanel/CodeEditor.vue
+++ src/components/tabPanel/CodeEditor.vue
@@ -1,19 +1,12 @@
 <template>
-  <div class="editor-grid">
-    <div class="line-numbers"></div>
-    <textarea
-      class="code-editor"
-      spellcheck="false"
-      v-model="this.tab.fileContent"
-    ></textarea>
-  </div>
+  <div ref="editor" class="editor"></div>
 </template>
-
 
 <script lang="ts">
 import { Options, Vue, setup } from "vue-class-component";
 import { useStore } from "@/store";
 import Tab from "@/controllers/Tab";
+import * as monaco from "monaco-editor";
 
 @Options({
   components: { CodeEditor },
@@ -25,8 +18,117 @@ export default class CodeEditor extends Vue {
   store = setup(() => useStore());
   tab!: Tab;
 
-  print() {
-    console.log(this.tab.fileContent);
+  mounted() {
+    let tab = this.tab;
+
+    var syntax = {
+      keywords: [
+        "begin",
+        "const",
+        "do",
+        "else",
+        "end",
+        "func",
+        "if",
+        "in",
+        "let",
+        "of",
+        "proc",
+        "record",
+        "then",
+        "type",
+        "var",
+        "while",
+        "putint"
+      ],
+      
+      typeKeywords: [
+        "Integer literal",
+        "Character literal",
+        "array",
+        "Integer",
+        "Char",
+        "String",
+        "Boolean",
+      ],
+
+
+      brackets: [
+        { open: "{", close: "}", token: "delimiter.curly" },
+        { open: "[", close: "]", token: "delimiter.bracket" },
+        { open: "(", close: ")", token: "delimiter.parenthesis" },
+      ],
+
+      tokenizer: {
+        root: [
+          { include: "@whitespace" },
+          { include: "@numbers" },
+          { include: "@strings" },
+
+          [/[.,:;]/, "delimiter"],
+          [/[{}[]()]/, "@brackets"],
+
+          [/@[a-zA-Z]\w*/, "tag"],
+          [/[a-zA-Z]\w*/,
+            {
+              cases: {
+                "@keywords": "keyword",
+                "@default": "identifier",
+              },
+            },
+          ],
+        ],
+
+        // Deal with white space, including single and multi-line comments
+        whitespace: [
+          [/\s+/, "white"],
+          [/(^!.*$)/, "comment"]
+        ],
+
+        // Recognize hex, negatives, decimals, imaginaries, longs, and scientific notation
+        numbers: [
+          [/-?0x([abcdef]|[ABCDEF]|\d)+[lL]?/, "number.hex"],
+          [/-?(\d*\.)?\d+([eE][+-]?\d+)?[jJ]?[lL]?/, "number"],
+        ],
+
+        // Recognize strings, including those broken across lines with \ (but not without)
+        strings: [
+          [/"$/, "string.escape", "@popall"],
+          [/"/, "string.escape", "@stringBody"],
+          [/"$/, "string.escape", "@popall"],
+          [/"/, "string.escape", "@dblStringBody"],
+        ],
+        stringBody: [
+          [/[^\\"]+$/, "string", "@popall"],
+          [/[^\\"]+/, "string"],
+          [/\\./, "string"],
+          [/"/, "string.escape", "@popall"],
+          [/\\$/, "string"],
+        ],
+        dblStringBody: [
+          [/[^\\"]+$/, "string", "@popall"],
+          [/[^\\"]+/, "string"],
+          [/\\./, "string"],
+          [/"/, "string.escape", "@popall"],
+          [/\\$/, "string"],
+        ],
+      },
+    } as any;
+
+    monaco.languages.register({ id: "triangle" });
+
+    monaco.languages.setMonarchTokensProvider("triangle", syntax);
+    let container = this.$refs["editor"] as HTMLElement;
+    let editor = monaco.editor.create(container, {
+      value: tab.fileContent,
+      language: "triangle",
+      theme: "vs-dark",
+      automaticLayout: true,
+    });
+
+    editor.onDidChangeModelContent(function () {
+      tab.fileContent = editor.getValue();
+    });
   }
 }
 </script>
